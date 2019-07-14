a24ccba3d1940e462317ff7241ce572d5e23c90a
Nate Jacobs <nathan.jacobs@chandleraz.gov>
2019-07-14 15:14:05 -0400

    Add the price resource.

---
+++ src/Resources/Price.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace NateJacobs\MurstenStock\Resources;
+
+use NateJacobs\MurstenStock\Request;
+use NateJacobs\MurstenStock\Response\Price as PriceResponse;
+use NateJacobs\MurstenStock\Exceptions\MissingParamsException;
+
+class Price extends Request
+{
+	public function getPrice($id, $type = 'set', $options = [])
+	{
+		$defaults = [
+			'guide_type' => 'sold',
+			'new_or_used' => 'N',
+		];
+
+		if (false === is_array($options)) {
+			return new MissingParamsException('The options provided must be an array.');
+		}
+
+		if (false !== in_array(strtoupper($type), ['MINIFIG, PART, SET, BOOK, GEAR, CATALOG, INSTRUCTION, UNSORTED_LOT, ORIGINAL_BOX'])) {
+			return new MissingParamsException('You must pass a valid item type.');
+		}
+
+		if (is_array($options)) {
+			$options = array_merge( $defaults, $options );
+			$options_string = http_build_query($options, '', '&');
+		}
+
+		try {
+			return $this->createReturnObject($this->request('get', 'items'.'/'.$type.'/'.$id.'/price?'.$options_string));
+		} catch(\Exception $e) {
+			return $e;
+		}
+	}
+	private function createReturnObject($response)
+	{
+		$response = is_array($response) ? $response : [$response];
+
+		foreach ($response as $object) {
+			$price[] = new PriceResponse($object);
+		}
+
+		return $price;
+	}
+}
---
+++ src/Response/Price.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace NateJacobs\MurstenStock\Response;
+
+class Price
+{
+	public function __construct($price)
+	{
+		$this->aggregatePrices = $this->setPrices($price);
+		$this->currencyCode = $price->currency_code;
+		$this->condition = $this->setCondition($price);
+		$this->unitQuantity = $price->unit_quantity;
+		$this->totalQuantity = $price->total_quantity;
+		$this->priceDetail = $price->price_detail;
+
+		return $this;
+	}
+
+	private function setPrices($price)
+	{
+		$formatter = new \NumberFormatter(setlocale(LC_ALL, 0), \NumberFormatter::CURRENCY);
+
+		$price_array = [
+			'averagePrice' => $formatter->formatCurrency(
+				$price->avg_price,
+				$price->currency_code
+			),
+			'maximumPrice' => $formatter->formatCurrency(
+				$price->max_price,
+				$price->currency_code
+			),
+			'minimumPrice' => $formatter->formatCurrency(
+				$price->min_price,
+				$price->currency_code
+			),
+			'quantityAveragePrice' => $formatter->formatCurrency(
+				$price->qty_avg_price,
+				$price->currency_code
+			),
+		];
+
+		return $price_array;
+	}
+
+	private function setCondition($price)
+	{
+		switch ($price->new_or_used) {
+			case 'N':
+				$condition = 'New';
+				break;
+			case 'U':
+				$condition = 'Used';
+				break;
+			default:
+				$condition = '';
+				break;
+		}
+
+		return $condition;
+	}
+}
